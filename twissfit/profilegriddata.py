680c3a2dd95dd033358b5d0dbe545d35635e2788
xaratustrah <shahab.sanjari@gmail.com>
2019-11-05 14:48:46 +0100

    changed x axis range to include real data not just indexes

--- twissfit/profilegriddata.py
+++ twissfit/profilegriddata.py
@@ -15,7 +15,7 @@ import sys
 from io import BytesIO
 import matplotlib.pyplot as plt
 
-RANGE = 30
+RANGE = 25
 SIGMA_ESTIMATE = 20
 
 
@@ -43,15 +43,19 @@ class ProfileGridData(object):
     @staticmethod
     def fit_and_plot(x_data, y_data, name=""):
 
+        # x and y are the variables for the fitter
+        # pos is the real position vector in mm
+        x = x_data
+        #delta_pos = np.diff(pos)[0]
         y = y_data
-        #x = x_data
-        #pos = x_data
-        x = np.arange(len(y))
+        #x = np.arange(len(y))
+        #x = pos
 
         # Estimate for mean and sigma
-        mean = y.argmax()
+        mean_idx = y.argmax()
+        mean = x[mean_idx]
         sigma = SIGMA_ESTIMATE
-        offset = y[mean - 5]
+        offset = y[mean_idx - 5]
         slope = 1
         amp = 1
         p = [offset, slope, amp, mean, sigma]
@@ -61,18 +65,18 @@ class ProfileGridData(object):
         # fit
         popt, pcov = curve_fit(ProfileGridData.fit_function,
                                x[data_cut], y[data_cut], p0=p)
+        mean = popt[3]
+        sigma = popt[4]
 
-        # Get the area
         area = sum(ProfileGridData.fit_function(x, *popt))
-        print(popt)
         # plot with original data
         fig = plt.figure()
         ax = fig.gca()
         ax.plot(x, y, 'kx', label='Data')
         ax.plot(x[data_cut], ProfileGridData.fit_function(
             x[data_cut], *popt), 'r', label='Fit')
-        ax.set_xlabel('mu = {:0.2e}, sig = {:0.2e}, area = {:0.2e}'.format(
-            popt[3], popt[4], area))
+        ax.set_xlabel('mean = {:0.2e}, sigma = {:0.2e}, area = {:0.2e}'.format(
+            mean, sigma, area))
         ax.set_title(name)
 
         # Now add the legend with some customizations.
@@ -84,14 +88,16 @@ class ProfileGridData(object):
 
         plt.grid()
         plt.savefig('{}.pdf'.format(name))
-        print(name, ' '.join(map(str, popt)), area)
+        return popt, area
 
     def process_horiz_and_vert(self):
         self._read_data()
         pos = self.data[:, 0]
         hor_grid = self.data[:, 1]
         ver_grid = self.data[:, 2]
-        ProfileGridData.fit_and_plot(
+        popt, area = ProfileGridData.fit_and_plot(
             pos, hor_grid, name='{}_Horizontal'.format(self.filename_base))
-        ProfileGridData.fit_and_plot(
+        print(self.filename_base, ' | '.join(map(str, popt)), area)
+        popt, area = ProfileGridData.fit_and_plot(
             pos, ver_grid, name='{}_Vertical'.format(self.filename_base))
+        print(self.filename_base, ' | '.join(map(str, popt)), area)
